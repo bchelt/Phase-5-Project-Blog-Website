1f8393ab8769b2e63bf97b9480a20b7305abf842
Brandon Helt <brandon.helt@yahoo.com>
2022-09-28 18:00:06 -0700

    Fixed error when viewing post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -3,18 +3,21 @@ import { useParams } from 'react-router-dom';
 
 function Post() {
     const [post, setPost] = useState({})
+    const [user, setUser] = useState({})
     const { postId } = useParams()
 
     useEffect(() => {
         fetch(`http://localhost:3000/posts/${postId}`)
         .then((res) => res.json())
-        .then((data) => setPost(data))
+        .then((data) => {
+            setPost(data)
+            setUser(data.user)
+        })
     }, [])
-
     return (
         <div>
             <h1>{post.title}</h1>
-            <h4>By: {post.user.full_name}</h4>
+            <h4>By: {user.full_name}</h4>
             <div>{post.content}</div>
         </div>
     )
